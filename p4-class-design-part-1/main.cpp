4bd9e9b3a80939ba46d4f54570d1c165118d3d84
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2022-02-05 20:28:44 -0800

    Initial Commit

---
+++ p4-class-design-part-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  p4-class-design-part-1
+//
+//  Created by Jake Short on 2/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
